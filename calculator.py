91beb27077a4a84ceb6c2d9eec2ab454c0432095
far79 <mohammedfarhanahmed305@gmail.com>
2026-07-12 21:32:35 +0530

    improved code

--- calculator.py
+++ calculator.py
@@ -1,41 +1,36 @@
+def add(x,y):
+	return (x+y)
 
-# 	return (x+y)
+def sub(x,y):
+	return(x-y)
 
-# def sub(x,y):
-# 	return(x-y)
+def mul(x,y):
+	return(x*y)
 
-# def mul(x,y):
-# 	return(x*y)
+def div(x,y):
+	return(x/y)
 
-# def div(x,y):
-# 	return(x/y)
+print("choose a operation to perform by number:\n","1)add\n2)sub\n3)mul\n4)div",sep="")
 
-# print("choose a operation to perform by number:\n","1)add\n2)sub\n3)mul\n4)div",sep="")
-# a=int(input("enter operation number:"))
+b="yes"
 
-# while True:
-# 	if(a==1):
-# 		res=add(float(input("enter first number:")),float(input("enter second number:")))
-# 		print(res)
-# 	elif(a==2):
-# 		res=sub(float(input("enter first number:")),float(input("enter second number:")))
-# 		print(res)
-# 	elif(a==3):
-# 		res=mul(float(input("enter first number:")),float(input("enter second number:")))
-# 		print(res)
-# 	elif(a==4):
-# 		res=div(float(input("enter first number:")),float(input("enter second number:")))
-
-
-
-p=float(input("enter value of pi:"))
-if(p==""):
-	p=3.14
-
-print(p)
+while (b=="yes"):
+	a=int(input("enter operation number:"))
+	if(a==1):
+		res=add(float(input("enter first number:")),float(input("enter second number:")))
+		print(res)
+	elif(a==2):
+		res=sub(float(input("enter first number:")),float(input("enter second number:")))
+		print(res)
+	elif(a==3):
+		res=mul(float(input("enter first number:")),float(input("enter second number:")))
+		print(res)
+	elif(a==4):
+		res=div(float(input("enter first number:")),float(input("enter second number:")))
+		print(res)
+	b=input("Do you want to continue calculation(yes/no)")
 
 
-(s*(s-a)*(s-b)*(s-c))**0.5
 
 
 
