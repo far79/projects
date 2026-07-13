9f8f35d9f908868653c1199b67bbd002946852c3
far79 <mohammedfarhanahmed305@gmail.com>
2026-07-13 15:29:07 +0530

    made log text file

--- log_reader/log_reader.py
+++ log_reader/log_reader.py
@@ -1,25 +1,34 @@
-log = "2026-07-13 14:02:15 | ERROR | /api/login | 500 | 104ms"
+# /home/mm/projects/log_reader/logs
 
-log_finder = log.find("|")#for first "|"
-date = log[:log_finder]
+with open("/home/mm/projects/log_reader/logs","r") as file:
+	lines = file.readlines()
+	lines_counts= len(lines)
 
+i=0
+while i<=lines_counts-1:
+	log = lines[i]
 
-log1 = log[log_finder+2:]#for second "|"
-log1_finder = log1.find("|")
-info = log1[:log1_finder]
-
-
-log2 = log1[log1_finder+2:]
-log2_finder = log2.find("|")
-type = log2[:log2_finder]
+	log_finder = log.find("|")#for first "|"
+	date = log[:log_finder]
 
 
-log3 = log2[log2_finder+2:]
-log3_finder = log3.find("|")
-status=log3[:log3_finder]
+	log1 = log[log_finder+2:]#for second "|"
+	log1_finder = log1.find("|")
+	info = log1[:log1_finder]
 
 
-log4 = log3[log3_finder+2:]
-latency=log4
+	log2 = log1[log1_finder+2:]
+	log2_finder = log2.find("|")
+	type = log2[:log2_finder]
 
-print(date, info, type, status, latency)
+
+	log3 = log2[log2_finder+2:]
+	log3_finder = log3.find("|")
+	status=log3[:log3_finder]
+	
+
+	log4 = log3[log3_finder+2:]
+	latency=log4
+
+	print(date, info, type, status, latency)
+	i+=1
