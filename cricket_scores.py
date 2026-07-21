b33a2517692fdac3e2cd1e2113491e65a2ab62d3
far79 <mohammedfarhanahmed305@gmail.com>
2026-07-21 21:00:54 +0530

    Optimize async DNS checks and DRY up email validation logic

--- cricket_scores.py
+++ cricket_scores.py
@@ -1,32 +1,48 @@
 total_runs = 0
 wickets = 0
 overs = 0
-balls = 0
+balls = 1
 
 update = input("Press enter to continue:")
 
 while (update == "") or (update == "enter"):
+
+    runs = input("enter run on this ball or type 'w' for wicket or 'wide' for wide ball:")
+
+    # if (runs == "no ball"):
+    #     pass
+
+    # else:
+    #     if balls == 6:
+    #         overs+=1
+    #         balls = 0
+    #     print(overs,".",balls,sep="")
+    
     if balls == 6:
         overs+=1
         balls = 0
     print(overs,".",balls,sep="")
+
+    if (runs == "no ball"):
+        print("free hit")
+        print(overs,".",balls,"f",sep="")
     
 
-    runs = input("enter run on this ball or type 'w' for wicket or 'wide' for wide ball:")
-
-    if (runs == "w"):
+    elif (runs == "w"):
         wickets+=1
         balls +=1
         print("total wickets are:",wickets)
 
     elif (runs == "wide"):
         total_runs +=1
-        print("total runs:",total_runs)
+        print("total runs:",total_runs-1)
 
     else:
         runs = int(runs)
         total_runs +=runs 
-        print("total runs:",total_runs)
         balls += 1
+        print("total runs:",total_runs)
+        
+
 
 
